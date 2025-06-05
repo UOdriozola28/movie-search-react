3ba34e187d2f3f19d46452400823b774a94c93fd
Walin Paredes <paolitomaldonadofarje@gmail.com>
2025-06-04 19:49:34 -0500

    Adding an image not found to movies with a source not found

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,16 +1,26 @@
+import noImage from './../assets/images/no-image.png'
+
 function ListOfMovies({ movies }) {
     return (
         <ul className="movies">
             {
-                movies.map(movie => (
-                    <li className="movie" key={movie.id}>
-                        <h3>{movie.title}</h3>
-                        <p>{movie.year}</p>
-                        <img src={movie.image} alt={movie.title} />
-                    </li>
-                ))
+                movies.map(movie => {
+                    return (
+                        <li className="movie" key={movie.id} >
+                            <h3>{movie.title}</h3>
+                            <p>{movie.year}</p>
+                            <img
+                                src={movie.image}
+                                onError={(e) => {
+                                    e.target.src = noImage
+                                }}
+                                alt={movie.title}
+                            />
+                        </li>
+                    )
+                })
             }
-        </ul>
+        </ul >
     )
 }
 
